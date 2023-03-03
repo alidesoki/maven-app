2ab3715ed7f7eafa7c94bdaf83796f728b59a543
alidesoki96 <alidesoki96@gmail.com>
2023-03-03 20:58:38 +0200

    adding a jenkins file

--- script.groovy
+++ script.groovy
@@ -10,7 +10,7 @@ def buildImage() {
     //     sh "echo $PASS | docker login -u $USER --password-stdin"
     //     sh 'docker push nanajanashia/demo-app:jma-2.0'
     }
-} 
+ 
 
 def deployApp() {
     echo 'deploying the application...'
